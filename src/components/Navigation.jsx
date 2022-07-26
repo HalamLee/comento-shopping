48167423706eeaf0a6335afb8caf330f918f3e94
Lami <Halam.Lee.Lami@gmail.com>
2022-07-26 15:27:04 +0900

    feat: 네비게이션 이전 버튼 기능 구현

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,40 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
-const Navigation = () => {
+const Navigation = ({ isPreBtn }) => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <Wrapper>
+      {isPreBtn && (
+        <PreBtn onClick={() => navigate('/')}>
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </PreBtn>
+      )}
       <Title>코멘토 쇼핑</Title>
-    </div>
+    </Wrapper>
   );
 };
 
 export default Navigation;
 
+const Wrapper = styled.div`
+  height: 63px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+const PreBtn = styled.div`
+  position: absolute;
+  left: 33px;
+  font-size: 24px;
+  cursor: pointer;
+`;
 const Title = styled.div`
-  width: 95px;
-  height: 26px;
   font-family: 'Noto Sans KR', sans-serif;
   font-weight: 700;
   font-size: 18px;
   line-height: 26.06px;
-  text-align: center;
-  padding: 20px 151px 17px;
 `;
